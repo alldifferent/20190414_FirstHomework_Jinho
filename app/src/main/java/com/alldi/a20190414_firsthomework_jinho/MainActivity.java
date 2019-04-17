0e25958bcfc8e146db5cc6763c6b1e020a4ea119
alldifferent <alldifferent@naver.com>
2019-04-17 18:14:27 +0900

    자바 Html코드로 Text 부분 색상 change작업

--- app/src/main/java/com/alldi/a20190414_firsthomework_jinho/MainActivity.java
+++ app/src/main/java/com/alldi/a20190414_firsthomework_jinho/MainActivity.java
@@ -2,12 +2,32 @@ package com.alldi.a20190414_firsthomework_jinho;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox agreementCkBx;
+    CheckBox agreementCkBx1;
+    CheckBox agreementCkBx2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        agreementCkBx = findViewById(R.id.agreementCkBx);
+        agreementCkBx1 = findViewById(R.id.agreementCkBx1);
+        agreementCkBx2 = findViewById(R.id.agreementCkBx2);
+
+        String text = "<font color = 'black'>서비스 이용약관</font><font color = '#7848d5'>(필수)</font>";
+        String text1 = "<font color = 'black'>개인정보처리방침</font><font color = '#7848d5'>(필수)</font>";
+        String text2 = "<font color = 'black'>유료상품 이용약관</font><font color = '#7848d5'>(필수)</font>";
+
+        agreementCkBx.setText(Html.fromHtml(text), TextView.BufferType.SPANNABLE);
+        agreementCkBx1.setText(Html.fromHtml(text), TextView.BufferType.SPANNABLE);
+        agreementCkBx2.setText(Html.fromHtml(text), TextView.BufferType.SPANNABLE);
     }
 }
